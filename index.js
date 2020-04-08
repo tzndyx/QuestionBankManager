1f1573cc3152e3749d15ee658ee5e6aea675e7fa
tzndyx <35186615+tzndyx@users.noreply.github.com>
2020-04-08 21:19:04 +0800

    新增对试题的增删改查

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 /**
  * @author tongzn
  */
-// (function() {
-//     localStorage.clear();
-//
-//     // 插入测试数据
-//     localStorage.setItem('doEdit', this.doEdit);
-// })()
-
 var QBMsys = angular.module('QBMsys', []);
 QBMsys.controller("loginCtrl", loginCtrl);
 
--- plugin/main.js
+++ plugin/main.js
@@ -6,23 +6,29 @@ function f() {
     // history.go(0)
     setInterval(function () {
         location.reload()
-    },2000)
+    }, 2000)
 }
 
 const QBMsys = angular.module('QBMsys', []);
+//过滤器-最后更新时间 时间戳转日期格式
+QBMsys.filter('updateTime', function () {
+    return function (text) {
+        return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
+    }
+});
 //注入公共方法，塞入路由传参
-const injectCommon = function (obj) {
+const injectCommon = (obj) => {
     console.log('info>> inject started')
     var baseUrl = '../../';
     var commonShowcompleteflag = true;
-    obj.goto = function (target,params) {
+    obj.goto = function (target, params) {
         let route = router[target];
-        params && sessionStorage.setItem('urlparams',JSON.stringify(params));
+        params && sessionStorage.setItem('urlparams', JSON.stringify(params));
         window.location.href = baseUrl + route.url;
     }
-    obj.goreplace = function (target,params) {
+    obj.goreplace = function (target, params) {
         let route = router[target];
-        params && sessionStorage.setItem('urlparams',JSON.stringify(params));
+        params && sessionStorage.setItem('urlparams', JSON.stringify(params));
         window.location.replace(baseUrl + route.url)
     }
     obj.goback = function (steps) {
@@ -36,89 +42,294 @@ const injectCommon = function (obj) {
     }
     //悬浮菜单的方法
     obj.commonShowcomplete = function () {
-        if(commonShowcompleteflag){
+        if (commonShowcompleteflag) {
             commonShowcompleteflag = false;
             setTimeout(function () {
                 commonShowcompleteflag = true
-            },500)
-            $(".common_shortcuts").animate({'right':'0'},300);
+            }, 500)
+            $(".common_shortcuts").animate({'right': '0'}, 300);
         }
     }
     obj.commonHidecomplete = function () {
-        $(".common_shortcuts").animate({'right':'-1.4rem'},300);
+        $(".common_shortcuts").animate({'right': '-1.4rem'}, 300);
     }
     obj.backTOtop = function () {
         document.body.scrollTop = document.documentElement.scrollTop = 0;
     }
-    obj.goToHome = function(){
+    obj.goToHome = function () {
         obj.goto('teacher')
     }
     obj.goToMsg = function () {
 
     }
-    obj.commonToggleMenu = function (selector,flag) {
+    obj.commonToggleMenu = function (selector, flag) {
         flag && $(selector).slideDown(400);
         flag || $(selector).slideUp(400);
     }
     try {
         var params = JSON.parse(sessionStorage.getItem('urlparams'));
-        for(let i in params){
+        for (let i in params) {
             obj[i] = params[i];
         }
         sessionStorage.removeItem('urlparams');
-    }catch (e) {
+    } catch (e) {
         console.error('获取路由跳转时传递的值失败，请检查参数格式是否正确')
     }
     console.log('info>> inject finished')
     // TODO 设置openList
     // 'login.html' != window.location.pathname.split('/').reverse()[0] && sessionStorage.getItem('loginFlag') == null && obj.goto('login')
 }
+//常用工具方法模块
+const QBMsysUtils = {
+    // 获取时间戳
+    'getTimeStamp': () => {
+        return new Date().getTime()
+    },
+    // 转化需要保存和获取的数组跟JOSN对象  （都转为字符串）
+    'saveArray': (arr) => {
+        return arr.join(';');
+    },
+    'getArray': (string) => {
+        return string.split(';');
+    },
+    'saveJson': (json) => {
+        return JSON.stringify(json);
+    },
+    'getJson': (string) => {
+        return JSON.parse(string);
+    },
+    //获取试题和试卷模板
+    'getQuestionTemplate': () => {
+        return {
+            id: '',
+            lastUpdate: "",
+            title: "",
+            type: "",
+            descripe: "",
+            options: [],//以下五个字符串转数组
+            answer: []
+        }
+    },
+    'getPaperTemplate': () => {
+        return {
+            id: '',
+            lastUpdate: "",
+            title: "",
+            descripe: "",
+            choiceQuestion: [],  //以下五个字符串转数组
+            fillblankQuestion: [],
+            judgementQuestion: [],
+            explanQuestion: [],
+            shortanswerQuestion: []
+        }
+    }
+}
 
-/*TODO
-*question对象
-* {
-    id:'时间戳',
-    title:"",
-    type:"",
-    descripe:"",
-    options:[],
-    answer:[]
+// 操作试题的模块：新建试题保存   修改试题保存  审核试题保存  删除试题    查询试题
+const operateQuestion = {
+    'create': (question) => {
+        // 保存对象到本地
+        question.options && (options.options = QBMsysUtils.saveArray(options.options))
+        question.answer && (options.answer = QBMsysUtils.saveArray(options.answer))
+        question.id = QBMsysUtils.getTimeStamp()
+        question.lastUpdate = QBMsysUtils.getTimeStamp();
+        let key = question.id;
+        let value = QBMsysUtils.saveJson(question)
+        window.localStorage.setItem(key, value)
+        // 保存id到 questionCreate
+        let questionCreate;
+        try {
+            questionCreate = QBMsysUtils.getArray(window.localStorage.getItem('questionCreate'))
+        } catch (e) {
+            questionCreate = []
+        }
+        questionCreate.push(key);
+        window.localStorage.setItem('questionCreate', QBMsysUtils.saveArray(questionCreate));
+    },
+    'update': (id, question) => {
+        // 保存对象到本地
+        question.options && (options.options = QBMsysUtils.saveArray(options.options))
+        question.answer && (options.answer = QBMsysUtils.saveArray(options.answer))
+        question.id = id + "-1";
+        question.lastUpdate = QBMsysUtils.getTimeStamp();
+        let key = question.id;
+        let value = QBMsysUtils.saveJson(question)
+        window.localStorage.setItem(key, value)
+        // 保存id到 questionUpdate
+        let questionUpdate;
+        try {
+            questionUpdate = QBMsysUtils.getArray(window.localStorage.getItem('questionUpdate'))
+        } catch (e) {
+            questionUpdate = []
+        }
+        questionUpdate.push(key);
+        window.localStorage.setItem('questionUpdate', QBMsysUtils.saveArray(questionUpdate));
+    },
+    'examine': (id, flag) => { //审核对象id   审核通过 true     OR     拒绝 false
+        if (-1 != id.indexOf('-1')) {//修改待审核
+            // 从更新队列中删除
+            let questionUpdate = QBMsysUtils.getArray(window.localStorage.getItem('questionUpdate'));
+            questionUpdate.splice(questionUpdate.indexOf(id), 1);
+
+            if (flag) {
+                // 塞入完成队列中
+                let key = id.split('-')[0];
+                let questionFinished;
+                try {
+                    questionFinished = QBMsysUtils.getArray(window.localStorage.getItem('questionFinished'))
+                } catch (e) {
+                    questionFinished = []
+                }
+                questionFinished.push(key)
+                // 更新本地存储的数据
+                window.localStorage.setItem(key, window.localStorage.getItem(id))
+            }
+            window.localStorage.removeItem(id);
+        } else {//新建待审核
+            // 从新建队列中删除
+            let questionCreate = QBMsysUtils.getArray(window.localStorage.getItem('questionCreate'));
+            questionCreate.splice(questionCreate.indexOf(id), 1);
+            if (flag) {
+                // 塞入完成队列中
+                let questionFinished;
+                try {
+                    questionFinished = QBMsysUtils.getArray(window.localStorage.getItem('questionFinished'))
+                } catch (e) {
+                    questionFinished = []
+                }
+                questionFinished.push(key)
+                // 本地数据已存在，不需要更新
+                return
+            }
+            window.localStorage.removeItem(id);
+        }
+    },
+    'delete': (id) => {
+        let questionFinished;
+        try {
+            questionUpdate = QBMsysUtils.getArray(window.localStorage.getItem('questionUpdate'))
+        } catch (e) {
+            questionUpdate = []
+        }
+        questionFinished.splice(questionFinished.indexOf(id), 1);
+        window.localStorage.removeItem(id);
+    },
+    // 根据id获取试题对象
+    'query': (id) => {
+        try {
+            let question = QBMsysUtils.getJson(localStorage.getItem(id))
+            question.options && QBMsysUtils.getArray(question.options)
+            question.answer && QBMsysUtils.getArray(question.answer)
+            return QBMsysUtils.getJson(localStorage.getItem(id))
+        } catch (e) {
+            console.error('QBMsys logInfo >> 试题：' + id + ' 查询失败')
+        }
+    }
 }
-*
-*paper对象
-* {
-    id:'时间戳',
-    title:"",
-    descripe:"",
-    choiceQuestion:[],
-    fillblankQuestion:[],
-    judgementQuestion:[],
-    explanQuestion:[],
-    shortanswerQuestion:[]
+// 操作试卷的模块：新建试卷保存   修改试卷保存  审核试卷保存  删除试卷    查询试卷
+const savePaper = {
+    'create': () => {
+    },
+    'update': () => {
+    },
+    'examine': () => {
+    },
+    'delete': () => {
+    },
+    // 根据id获取试卷对象
+    'query': (id) => {
+        function getJsonById(attribute) {
+            let arr = QBMsysUtils.getArray(attribute)
+            let arr2 = [];
+            for (index in arr) {
+                arr2.push(operateQuestion.query(arr[index]))
+            }
+            return arr2
+        }
+
+        try {
+            let paper = localStorage.getItem(id);
+            paper.choiceQuestion = getJsonById(paper.choiceQuestion);
+            paper.fillblankQuestion = getJsonById(paper.fillblankQuestion);
+            paper.judgementQuestion = getJsonById(paper.judgementQuestion);
+            paper.explanQuestion = getJsonById(paper.explanQuestion);
+            paper.shortanswerQuestion = getJsonById(paper.shortanswerQuestion);
+            return paper
+        } catch (e) {
+            console.error('QBMsys logInfo >> 试卷：' + id + ' 查询失败')
+        }
+    }
 }
-*
-* question表--新建--questionAdd
-* question表--修改--questionUpdate
-* question表--完成--questionFinished
-*
-* paper表--新建--paperAdd
-* paper表--修改--paperUpdate
-* paper表--完成--paperFinished
-*
-* 表中存储的唯一索引为对象id，不同表的索引可以重复，如修改和完成
-* localStroge存储的 key为时间戳 value为 question和paper对象
-* 查询的对象为完成表   审核的对象为 新建表和修改表
-*
-* 新建的流程     创建对象 id为时间戳 ==> id保存到新建表（数组） ==> 对象存储到本地，key为id
-* 审核的流程     获取并遍历新建表（数组）的id，得到所有新建的对象 ==>
-*                   同意：id保存到完成表（数组），并从新建表（数组）中删除对应的id
-*                   拒绝：根据id删除本地存储的对象，并从新建表（数组）中删除对应的id
-*
-* 修改的流程     创建修改的对象，id为原id ==> id保存到修改表（数组） ==> 对象存储到本地，key为id-1（本地存储的key值不能重复）
-* 审核的流程     获取并遍历修改表（数组）的id-1，得到所有修改的对象 ==>
-*                   同意：对象存储到本地，key为id，removeItem（id+'-1'）删除本地存储的修改对象，并从修改表（数组）中删除对应的id
-*                   拒绝：removeItem（）删除本地存储的对象，并从修改表（数组）中删除对应的id
-*
-* 查询的流程     获取并遍历完成表（数组）的id，得到所有处理完成的对象
-*
-* */
+
+    /*TODO
+    *question对象
+    * {
+        id:'时间戳',
+        lastUpdate："修改提交的时间--新建提交、修改提交、审核提交",
+        title:"",
+        type:"",
+        descripe:"",
+        options:"",//以下五个字符串转数组
+        answer:""
+    }
+    *
+    *paper对象
+    * {
+        id:'时间戳',
+        lastUpdate："修改提交的时间--新建提交、修改提交、审核提交",
+        title:"",
+        descripe:"",
+        choiceQuestion:"",  //以下五个字符串转数组
+        fillblankQuestion:"",
+        judgementQuestion:"",
+        explanQuestion:"",
+        shortanswerQuestion:""
+    }
+    *
+    * question表--新建--questionCreate
+    * question表--修改--questionUpdate
+    * question表--完成--questionFinished
+    *
+    * paper表--新建--paperCreate
+    * paper表--修改--paperUpdate
+    * paper表--完成--paperFinished
+    *
+    * 表中存储的唯一索引为对象id，不同表的索引可以重复，如修改和完成
+    * localStorge存储的 key为时间戳 value为 question和paper对象
+    * 查询的对象为完成表   审核的对象为 新建表和修改表
+    * 表为数组，其保存的为对象（题目对象和试卷对象）的id
+    *
+    * 新建的流程     创建对象 id为时间戳 ==> id保存到新建表（数组） ==> 对象存储到本地，key为id
+    * 审核的流程     获取并遍历新建表（数组）的id，得到所有新建的对象 ==>
+    *                   同意：id保存到完成表（数组），并从新建表（数组）中删除对应的id
+    *                   拒绝：根据id删除本地存储的对象，并从新建表（数组）中删除对应的id
+    *
+    * 修改的流程     创建修改的对象，id为原id ==> id保存到修改表（数组） ==> 对象存储到本地，key为id-1（本地存储的key值不能重复）
+    * 审核的流程     获取并遍历修改表（数组）的id-1，得到所有修改的对象 ==>
+    *                   同意：对象存储到本地，key为id，removeItem（id+'-1'）删除本地存储的修改对象，并从修改表（数组）中删除对应的id
+    *                   拒绝：removeItem（）删除本地存储的对象，并从修改表（数组）中删除对应的id
+    *
+    * 查询的流程     获取并遍历完成表（数组）的id，得到所有处理完成的对象
+    *
+    * 本地存储的key值和对象的id是同一个值  修改表中的key值和id都为 原值 加 字符串 ‘-1’
+    *
+    * */
+
+    (function () {
+        localStorage.clear();
+        // 插入测试数据
+        let questionCreate = [];
+        let questionUpdate = [];
+        let questionFinished = [];
+
+        let paperCreate = [];
+        let paperUpdate = [];
+        let paperFinished = [];
+        localStorage.setItem('questionCreate', QBMsysUtils.saveArray(questionCreate));
+        localStorage.setItem('questionUpdate', QBMsysUtils.saveArray(questionUpdate));
+        localStorage.setItem('questionFinished', QBMsysUtils.saveArray(questionFinished));
+        localStorage.setItem('paperCreate', QBMsysUtils.saveArray(paperCreate));
+        localStorage.setItem('paperUpdate', QBMsysUtils.saveArray(paperUpdate));
+        localStorage.setItem('paperUpdate', QBMsysUtils.saveArray(paperUpdate));
+        localStorage.setItem('paperFinished', QBMsysUtils.saveArray(paperFinished));
+    })()
